e5ed3ae4197b23253743be0da0555192b8676369
Daria Daria <dariadaria@192.168.100.4>
2026-06-21 19:47:02 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let titleProjects = "Оценка стоимости"
+let screenValue = "Шаблонные, С уникальным дизайном, С анимациями"
+let screenPrice = 500
+let percentage = 20
+let fullPrice = 3000
+let responsive = true
